c0a2e5612c56d8dfa676b4da39b268cd9484826c
OceanJ <jiang.ocean@hotmail.com>
2015-10-20 23:58:45 +0800

    Add Test file for Binary Search Tree!
    Signed-off-by: OceanJ <jiang.ocean@hotmail.com>

--- Chapter 4 Tree/Tree/BinarySearchTree/TestBSTree.cpp
+++ Chapter 4 Tree/Tree/BinarySearchTree/TestBSTree.cpp
@@ -1,10 +1,34 @@
 #include "BinarySearchTree.h"
-void PrintTree(SearchTree T)
-{
-	
-}
+//void PrintTree(SearchTree T)
+//{
+//	if (!T)
+//	{
+//		cout << "\t\t"<<T->Element<<"\t\t";
+//		PrintTree(T->Left);
+//		PrintTree(T->Right);
+//	}
+//}
 int main()
 {
-	SearchTree TestTree;
-	
+	SearchTree TestTree=new TreeNode;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Insert " << i<<endl;
+		Insert(i, TestTree);
+		int succeed = Find(i, TestTree)!=NULL;
+		cout << "Succeed? " << succeed;
+	}
+	cout << "The Min is " << FindMin(TestTree)<<endl;
+	cout << "The Max is " << FindMax(TestTree);
+	for (int i = 5; i < 10; i++)
+	{
+		cout << "Delete " << i << endl;
+		Insert(i, TestTree);
+		int succeed = Find(i, TestTree) == NULL;
+		cout << "Succeed? " << succeed;
+	}
+	cout << "Make Empty!" << endl;
+	MakeEmpty(TestTree);
+	cout << "Succeed! Good bye!" << endl;
+
 }
