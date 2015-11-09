124408876af7803ce7318585f39d2af7e08deb8c
OceanJ <jiang.ocean@hotmail.com>
2015-11-09 23:54:03 +0800

    Create Quick Sort! Signed-off-by: OceanJ <jiang.ocean@hotmail.com>

---
+++ Chapter 7 Sorting/Sort/Quick Sort/Quick Sort.cpp
@@ -0,0 +1,13 @@
+#include "Quick Sort.h"
+ElementType CreatePivot(ElementType A[], int Left, int Right)
+{
+
+}
+void QSort(ElementType A[], int Left, int Right)
+{
+
+}
+void QuickSort(ElementType A[], int N)
+{
+	QSort(A, 0, N - 1);
+}
